2e8fbe48927af6db92cbd5aacade98aa20104264
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-01 21:52:19 +0300

    added api filtering by product Type

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,3 @@
-// const url =
-//     "https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=us&lang=en&currentpage=0&pagesize=30&categories=men_all&concepts=H%26M%20MAN";
-// const options = {
-//     method: "GET",
-//     headers: {
-//         "x-rapidapi-key": "b2e173c3a8msh95b68978cee8ee6p1ad865jsn6e796fcffc74",
-//         "x-rapidapi-host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com",
-//     },
-// };
-
-// async function getData() {
-//     try {
-//         const response = await fetch(url, options);
-//         const result = await response.text();
-//         console.log(result);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-// getData();
-
 // Cart Count
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 const cartCount = document.getElementById("cart-count");
--- products/script.js
+++ products/script.js
@@ -1,6 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
 const pageNum = urlParams.get("page") || 0;
-const url = `https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=us&lang=en&currentpage=${pageNum}&pagesize=30`;
+const ProductsType = urlParams.get("type");
+const baseUrl = 'https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list';
+const url = `${baseUrl}?country=us&lang=en&currentpage=${pageNum}&pagesize=30${ProductsType ? `&productTypes=${ProductsType}` : ''}`;
+
 const options = {
     method: "GET",
     headers: {
@@ -8,6 +11,7 @@ const options = {
         "x-rapidapi-host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com",
     },
 };
+
 const getProducts = async () => {
     try {
         const response = await fetch(url, options);
@@ -15,88 +19,88 @@ const getProducts = async () => {
         displayProducts(data.results);
         displayFilters(data.results);
         displayPagination(data.pagination);
-        console.log(data.results);
     } catch (error) {
         console.error(error);
+        alert("An error occurred while fetching the products.");
     }
 };
+
 const displayProducts = (products) => {
     const productListing = document.getElementById("product-listing");
-    productListing.innerHTML = products
-        .map(
-            (prod) => `
-            <div class="product-card" onclick="window.location.href='/product/?id=${prod.articles[0].code}'">
-                <img src="${prod.images[0].url}" alt="${prod.name}">
-                <div class="product-info">
-                    <span>${prod.categoryName}</span>
-                    <div class="card-text">
-                        <h4>${prod.name}</h4>
-                        <p>${prod.price.formattedValue}</p>
+    productListing.innerHTML = products.map(prod => `
+                <div class="product-card" onclick="window.location.href='/product/?id=${prod.articles[0].code}'">
+                    <img src="${prod.images[0].url}" alt="${prod.name}">
+                    <div class="product-info">
+                        <span>Category: ${prod.categoryName} ${prod.rgbColors
+            ? `<span class="prod-card-color" style="background-color:${prod.rgbColors[0]}"></span>${prod.rgbColors.length > 1 ? `+${prod.rgbColors.length}` : ""}`
+            : ""
+        }</span>
+                        <div class="card-text">
+                            <h4>${prod.name}</h4>
+                            <p>${prod.price.formattedValue}</p>
+                        </div>
                     </div>
                 </div>
-            </div>`
-        )
-        .join("");
+            `).join("");
 };
 
 const displayPagination = (pagination) => {
     const paginationContainer = document.querySelector(".pagination");
     let pageNumber = pagination.currentPage + 1;
-    let lastPage = pagination.numberOfPages + 1;
+    let lastPage = pagination.numberOfPages;
     paginationContainer.innerHTML = `
-        <span onclick="prevPage(${pagination.currentPage})" ${
-        pageNumber === 1 ? "style='display:none'" : ""
-    }><i class="fa-solid fa-angle-left"></i></span>
-        <span>${pageNumber}</span>
-        <span onclick="nextPage(${pagination.currentPage})" ${
-        pageNumber === lastPage ? "style='display:none'" : ""
-    }><i class="fa-solid fa-angle-right"></i></span>
-    `;
+                <span onclick="changePage(${pagination.currentPage - 1}, '${ProductsType || ''}')" ${pageNumber === 1 ? "style='display:none'" : ""}><i class="fa-solid fa-angle-left"></i></span>
+                <span>${pageNumber}</span>
+                <span onclick="changePage(${pagination.currentPage + 1}, '${ProductsType || ''}')" ${pageNumber === lastPage ? "style='display:none'" : ""}><i class="fa-solid fa-angle-right"></i></span>
+            `;
 };
-const nextPage = (pageNumber) => {
-    window.location.href = `/products/?page=${pageNumber + 1}`;
-};
-const prevPage = (pageNumber) => {
-    window.location.href = `/products/?page=${pageNumber - 1}`;
+
+const changePage = (pageNumber, type) => {
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', pageNumber);
+    if (type) {
+        url.searchParams.set('type', type);
+    } else {
+        url.searchParams.delete('type');
+    }
+    window.location.href = url.toString();
 };
 
 const displayFilters = (products) => {
-    const categoryCounts = {};
-    products.forEach((prod) => {
-        if (categoryCounts[prod.categoryName]) {
-            categoryCounts[prod.categoryName]++;
-        } else {
-            categoryCounts[prod.categoryName] = 1;
-        }
-    });
+    const categoryCounts = products.reduce((counts, prod) => {
+        counts[prod.categoryName] = (counts[prod.categoryName] || 0) + 1;
+        return counts;
+    }, {});
 
-    const categories = Object.keys(categoryCounts).map((category) => {
+    const categories = Object.keys(categoryCounts).map(category => {
         return { name: category, count: categoryCounts[category] };
     });
 
     const categoriesFilter = document.querySelector(".filter-section.category");
     categoriesFilter.innerHTML = `
-        <h4 class="filter-header" onclick="collapseFilter(event)">Category<span><i class="fa-solid fa-angle-right"></i></span></h4>
-        <div class="filter-options">
-        ${categories
-            .map(
-                (category) =>
-                    `<label><input type="checkbox"/>${category.name} (<span style="color:blue;">${category.count}</span>)</label>`
-            )
-            .join("")}
-        </div>
-    `;
+                <h4 class="filter-header" onclick="collapseFilter(event)">Category<span><i class="fa-solid fa-angle-right"></i></span></h4>
+                <div class="filter-options">
+                    ${categories.map(category => `
+                        <label>
+                            <input type="checkbox"/> ${category.name} (<span style="color:blue;">${category.count}</span>)
+                        </label>
+                    `).join("")}
+                </div>
+            `;
 };
 
 const collapseFilter = (e) => {
-    console.log(e);
     const content = e.target.nextElementSibling;
+    const icon = e.target.querySelector('i');
     if (content.classList.contains("extend")) {
+        icon.classList.replace("fa-angle-down", "fa-angle-right");
         content.classList.remove("extend");
         content.style.height = "0";
     } else {
+        icon.classList.replace("fa-angle-right", "fa-angle-down");
         content.classList.add("extend");
         content.style.height = content.scrollHeight + "px";
     }
 };
-getProducts();
+
+document.addEventListener('DOMContentLoaded', getProducts);
