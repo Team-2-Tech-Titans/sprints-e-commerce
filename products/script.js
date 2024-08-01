1cc7b6e501cef535e3e5ce34a8388d8296d5fad1
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-01 07:27:30 +0300

    paggination added

--- products/script.js
+++ products/script.js
@@ -13,7 +13,7 @@ const getProducts = async () => {
         const response = await fetch(url, options);
         const data = await response.json();
         displayProducts(data.results);
-        // displayPagination(data.pagination);
+        displayPagination(data.pagination);
         console.log(data.results);
     } catch (error) {
         console.error(error);
@@ -37,10 +37,25 @@ const displayProducts = (products) => {
         )
         .join("");
 };
-// const displayPagination = (pagination) => {
-//     const paginationContainer = document.querySelector(".pagination");
-//     const currentpage = pagination.currentPage;
-//     const totalPages = pagination.numberOfPages;
 
-// };
+const displayPagination = (pagination) => {
+    const paginationContainer = document.querySelector(".pagination");
+    let pageNumber = pagination.currentPage + 1;
+    let lastPage = pagination.numberOfPages + 1;
+    paginationContainer.innerHTML = `
+        <span onclick="prevPage(${pagination.currentPage})" ${
+        pageNumber === 1 ? "style='display:none'" : ""
+    }><i class="fa-solid fa-angle-left"></i></span>
+        <span>${pageNumber}</span>
+        <span onclick="nextPage(${pagination.currentPage})" ${
+        pageNumber === lastPage ? "style='display:none'" : ""
+    }><i class="fa-solid fa-angle-right"></i></span>
+    `;
+};
+const nextPage = (pageNumber) => {
+    window.location.href = `/products/?page=${pageNumber + 1}`;
+};
+const prevPage = (pageNumber) => {
+    window.location.href = `/products/?page=${pageNumber - 1}`;
+};
 getProducts();
