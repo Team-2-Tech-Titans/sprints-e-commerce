a9a026ac3e2dd1261ffc67535b945fe984a2d335
Ashraf Sayed <ashraf.neizk@gmail.com>
2024-08-04 12:55:52 +0300

    half the Checkout page done

--- checkout/script.js
+++ checkout/script.js
@@ -5,3 +5,13 @@ document.getElementById('country-select').addEventListener('change', function ()
         this.classList.remove('default')
     }
 })
+
+const toggleCheckoutForm = ()=>{
+    document.querySelector('.checkout-section').classList.toggle('payment-toggled')
+    const formToggleBtn = document.querySelector('.payment-btn.form-toggeler span')
+    if (formToggleBtn.innerText === 'Payment') {
+        formToggleBtn.innerText = 'Shipping'
+    }else{
+        formToggleBtn.innerText = 'Payment'
+    }
+}
